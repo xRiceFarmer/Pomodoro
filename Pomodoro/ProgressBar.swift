e9036c670558522d0ffe192720d36289efbee053
xRiceFarmer <khangthaidonguyen@gmail.com>
2024-01-30 16:20:23 +0700

    something wrong with background timer

---
+++ Pomodoro/ProgressBar.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressBar.swift
+//  Pomodoro
+//
+//  Created by Thái Khang on 30/01/2024.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressBar()
+}
