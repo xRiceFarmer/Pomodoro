95569493bf2360cd73802d74bf4f5ad9ae66bf88
xRiceFarmer <khangthaidonguyen@gmail.com>
2024-02-02 13:44:03 +0700

    added progressview

--- TimerWidget/TimerWidget.swift
+++ TimerWidget/TimerWidget.swift
@@ -7,20 +7,76 @@ struct TimerWidget: Widget {
     var body: some WidgetConfiguration {
         ActivityConfiguration(for: TimerAttributes.self) { context in
             TimerWidgetView(context: context)
+                .activityBackgroundTint(Color.white.opacity(0.25))
+
         } dynamicIsland: { context in
             DynamicIsland {
                 DynamicIslandExpandedRegion(.leading){
-                    Text.init(timerInterval:  Date.now...Date(timeInterval: Double(context.state.secondsRemaining), since: .now), pauseTime: context.state.endTime)
-                        .fontWeight(.bold)
+                    VStack{
+                        ProgressView(
+                            timerInterval: Date.now...Date(timeInterval: Double(context.state.secondsRemaining), since: .now),
+                            countsDown: true,
+                            label: { EmptyView() },
+                            currentValueLabel: {
+                                if context.state.sessionName == "Pomodoro"{
+                                    Text("PM")
+                                        .frame(maxWidth: .minimum(20, 20))
+                                }
+                                if context.state.sessionName == "Short Break"{
+                                    Text("SB")
+                                        .frame(maxWidth: .minimum(20, 20))
+
+                                }
+                                if context.state.sessionName == "Long Break"{
+                                    Text("LB")
+                                        .frame(maxWidth: .minimum(20, 20))
+
+                                }
+                            }
+                        )
+                        .progressViewStyle(.circular)
+                        .tint(.pink)
+                        .frame(maxWidth: .minimum(50, 50), alignment: .trailing)
+                    }
+                    
                 }
                 DynamicIslandExpandedRegion(.trailing){
-                    Text(context.state.sessionName)
-                        .fontWeight(.bold)
+                    VStack{
+                        
+                        Text.init(timerInterval:  Date.now...Date(timeInterval: Double(context.state.secondsRemaining), since: .now), pauseTime: context.state.endTime)
+                            .fontWeight(.bold)
+                            .frame(maxWidth: .minimum(50, 50), alignment: .center)
+                    }
+                       
                 }
             } compactLeading: {
-                Text(context.state.sessionName)
+                ProgressView(
+                    timerInterval: Date.now...Date(timeInterval: Double(context.state.secondsRemaining), since: .now),
+                    countsDown: true,
+                    label: { EmptyView() },
+                    currentValueLabel: {
+                        if context.state.sessionName == "Pomodoro"{
+                            Text("PM")
+                                .frame(maxWidth: .minimum(20, 20))
+                        }
+                        if context.state.sessionName == "Short Break"{
+                            Text("SB")
+                                .frame(maxWidth: .minimum(20, 20))
+
+                        }
+                        if context.state.sessionName == "Long Break"{
+                            Text("LB")
+                                .frame(maxWidth: .minimum(20, 20))
+
+                        }
+                    }
+                )
+                .progressViewStyle(.circular)
+                .tint(.pink)
+                .frame(maxWidth: .minimum(50, 50), alignment: .trailing)
             } compactTrailing: {
                 Text.init(timerInterval:  Date.now...Date(timeInterval: Double(context.state.secondsRemaining), since: .now), pauseTime: context.state.endTime)
+                    .frame(maxWidth: .minimum(50, 50), alignment: .leading)
             } minimal: {
            
             }
@@ -32,11 +88,14 @@ struct TimerWidgetView: View {
     let context: ActivityViewContext<TimerAttributes>
     var body: some View {
             HStack{
+                Text(context.state.sessionName)
+                    .frame(alignment: .leading)
+                    .padding()
                 Text.init(timerInterval:  Date.now...Date(timeInterval: Double(context.state.secondsRemaining), since: .now), pauseTime: context.state.endTime)
                     .font(.headline)
+                    .frame(alignment: .trailing)
                     .padding()
-                Text(context.state.sessionName)
-                    .padding()
+                
             }
     }
 }
